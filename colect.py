f646621e722d949334a39cbbcd206cc9ee9c7ec3
Reginaldo Santos <reginaldofuture@gmail.com>
2019-05-07 09:30:54 -0300

    Sinopse: Correção campo faltante em alguns filmes

--- colect.py
+++ colect.py
@@ -6,13 +6,15 @@ from imdb import IMDb,IMDbError
 ia = IMDb()
 
 # get a movie
-movie = ia.search_movie('Grumpier Old Men (1995)')
-movie = ia.get_movie(movie[0].movieID)
-print(movie.data)
+#movie = ia.search_movie('Grumpier Old Men (1995)')
+#movie = ia.get_movie(movie[0].movieID)
+#print(movie.keys())
+#if 'plot outline' in movie.keys():
+#	print('Esta na lista')
+#else:
+#	print('Não esta na lista')
 
-
-exit()
-import csv
+#exit()
 data = []
 with open('./dt_small/movies.csv', mode='r') as csv_file:
 		csv_reader = csv.DictReader(csv_file)
@@ -26,10 +28,14 @@ with open('./dt_small/movies.csv', mode='r') as csv_file:
 					movie = ia.search_movie(row["title"])
 					mvId = movie[0].movieID
 					movie = ia.get_movie(mvId)
-					synopsis = movie['synopsis'][0]
+					if 'synopsis' in movie.keys():
+						synopsis = movie['synopsis'][0]
+					elif 'plot outline' in movie.keys():
+						synopsis = movie['plot outline']
 					writer.writerow({'movieID':row['movieId'],'synopsis':synopsis})
-#		    	data.append((row['movieId'],synopsis))
 				except Exception as e:
 					print("Error:",e)
-				if row['movieId'] == '5':
-					break
+
+#Descomentar para parar
+#				if row['movieId'] == '5':
+#					break
